d76831fc74643ce6574b4f2cfdd95bb9f758c940
Seravielle-de-Lochan <jo.golembiewski@gmail.com>
2026-09-17 23:03:51 +0100

    Update offline cache for Courtship Codex v2

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-const CACHE="rook-courtship-codex-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+const CACHE="rook-courtship-codex-v2";
+const ASSETS=["./","./index.html","./styles.css","./data-core.js","./data-extra.js","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-    return resp;
+    const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return resp;
   }).catch(()=>caches.match("./index.html"))));
 });
